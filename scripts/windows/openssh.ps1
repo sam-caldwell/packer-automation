13bc424c5d7bb138a1570c1fceb4f937c20f3323
Sam Caldwell <mail@samcaldwell.net>
2020-01-04 17:27:02 -0600

    move scripts to windows directory and use microsoft-provided openssh versus provided one.

--- scripts/windows/openssh.ps1
+++ scripts/windows/openssh.ps1
@@ -1,3 +1,5 @@
+# See https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
+
 param (
   [switch]$AutoStart = $false
 )
@@ -5,21 +7,22 @@ param (
 Write-Output "AutoStart: $AutoStart"
 $is_64bit = [IntPtr]::size -eq 8
 
-# setup openssh
-$ssh_download_url = "https://www.mls-software.com/files/setupssh-7.1p1-1.exe"
+Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
 
-if (!(Test-Path "C:\Program Files\OpenSSH\bin\ssh.exe")) {
-    Write-Output "Downloading $ssh_download_url"
-    (New-Object System.Net.WebClient).DownloadFile($ssh_download_url, "C:\Windows\Temp\openssh.exe")
+# Install the OpenSSH Client
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 
-    # initially set the port to 2222 so that there is not a race
-    # condition in which packer connects to SSH before we can disable the service
-    Start-Process "C:\Windows\Temp\openssh.exe" "/S /port=2222 /privsep=1 /password=D@rj33l1ng" -NoNewWindow -Wait
-}
+# Install the OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-Stop-Service "OpenSSHd" -Force
+# Configure OpenSSH Server
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+Get-NetFirewallRule -Name *ssh*
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 
-# ensure vagrant can log in
+Stop-Service sshd
+
 Write-Output "Setting vagrant user file permissions"
 New-Item -ItemType Directory -Force -Path "C:\Users\vagrant\.ssh"
 C:\Windows\System32\icacls.exe "C:\Users\vagrant" /grant "vagrant:(OI)(CI)F"
@@ -31,9 +34,7 @@ Write-Output "Setting SSH home directories"
     Foreach-Object { $_ -replace '/home/(\w+)', '/cygdrive/c/Users/$1' } |
     Set-Content 'C:\Program Files\OpenSSH\etc\passwd'
 
-# Set shell to /bin/sh to return exit status
 $passwd_file = Get-Content 'C:\Program Files\OpenSSH\etc\passwd'
-$passwd_file = $passwd_file -replace '/bin/bash', '/bin/sh'
 Set-Content 'C:\Program Files\OpenSSH\etc\passwd' $passwd_file
 
 # fix opensshd to not be strict
@@ -42,10 +43,10 @@ $sshd_config = Get-Content "C:\Program Files\OpenSSH\etc\sshd_config"
 $sshd_config = $sshd_config -replace 'StrictModes yes', 'StrictModes no'
 $sshd_config = $sshd_config -replace '#PubkeyAuthentication yes', 'PubkeyAuthentication yes'
 $sshd_config = $sshd_config -replace '#PermitUserEnvironment no', 'PermitUserEnvironment yes'
+
 # disable the use of DNS to speed up the time it takes to establish a connection
 $sshd_config = $sshd_config -replace '#UseDNS yes', 'UseDNS no'
-# disable the login banner
-$sshd_config = $sshd_config -replace 'Banner /etc/banner.txt', '#Banner /etc/banner.txt'
+
 # next time OpenSSH starts have it listen on th eproper port
 $sshd_config = $sshd_config -replace 'Port 2222', "Port 22"
 Set-Content "C:\Program Files\OpenSSH\etc\sshd_config" $sshd_config
@@ -78,10 +79,10 @@ Set-Content C:\Windows\Temp\PATH ([byte[]][char[]] $env:PATH) -Encoding Byte
 
 # configure firewall
 Write-Output "Configuring firewall"
-netsh advfirewall firewall add rule name="SSHD" dir=in action=allow service=OpenSSHd enable=yes
-netsh advfirewall firewall add rule name="SSHD" dir=in action=allow program="C:\Program Files\OpenSSH\usr\sbin\sshd.exe" enable=yes
+
+netsh advfirewall firewall add rule name="SSHD" dir=in action=allow service=sshd enable=yes
 netsh advfirewall firewall add rule name="ssh" dir=in action=allow protocol=TCP localport=22
 
 if ($AutoStart -eq $true) {
-    Start-Service "OpenSSHd"
+    Start-Service sshd
 }
