fc5adb3df4262318f0ab3dc17de160e0ca46f423
Sam Caldwell <mail@samcaldwell.net>
2020-01-05 15:39:32 -0600

    fix errors in fetch-all.mk

--- Makefile.in/fetch-all.mk
+++ Makefile.in/fetch-all.mk
@@ -1,27 +1,27 @@
-MANIFEST='assets/manifest.yaml'
+MANIFEST='$(shell pwd)/assets/manifest.yml'
 fetch/iso:
 	@echo 'fetch iso images in $(MANIFEST)'
-	$(shell pwd)/scripts/packer/fetch-manifest.py --type iso --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type iso --manifest $(MANIFEST)
 
 fetch/windows:
 	@echo 'fetch windows assets from $(MANIFEST)'
-	$(shell pwd)/scripts/packer/fetch-manifest.py --type windows --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type windows --manifest $(MANIFEST)
 
 fetch/linux/deb:
 	MANIFEST=assets/linux/deb/manifest.yaml
-	$(shell pwd)/scripts/packer/fetch-manifest.py --type linux/deb --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type linux/deb --manifest $(MANIFEST)
 
 fetch/linux/rpm:
 	MANIFEST=assets/linux/rpm/manifest.yaml
-	$(shell pwd)/scripts/packer/fetch-manifest.py --type linux/rpm --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type linux/rpm --manifest $(MANIFEST)
 
 fetch/linux/src:
 	MANIFEST=assets/linux/src/manifest.yaml
-	$(shell pwd)/scripts/packer/fetch-manifest.py --type linux/src --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type linux/src --manifest $(MANIFEST)
 
 fetch/macos:
 	MANIFEST=assets/macos/manifest.yaml
-	$(shell pwd)/scripts/packer/fetch-manifest.py --type macos --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type macos --manifest $(MANIFEST)
 
 fetch/all:
 	@echo 'fetching all assets.'
