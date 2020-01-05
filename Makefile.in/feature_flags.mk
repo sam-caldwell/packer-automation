743fa760a4fe392066a1cb8a7b01e7657c579325
Sam Caldwell <mail@samcaldwell.net>
2020-01-05 12:31:24 -0600

    make BUILD_ON_* and PUSH_TO_* the prefixes for feature flag variables.  add to/local flag.

--- Makefile.in/feature_flags.mk
+++ Makefile.in/feature_flags.mk
@@ -1,44 +1,45 @@
 #
 # This is the current set of enabled feature flags.
 #
--include ff-on-aws.mk
--include ff-on-azure.mk
--include ff-on-parallels.mk
--include ff-on-vmware.mk
--include ff-to-vagrantup.mk
 
 on/virtualbox:
 	@echo 'enabling build-on virtualbox feature flag'
-	export ON_VIRTUALBOX=1
+	export BUILD_ON_PARALLELS=1
 
 on/vmware:
 	@echo 'enabling build-on vmware feature flag'
-	export ON_VMWARE=1
+	export BUILD_ON_PARALLELS=1
 	@echo 'not implemented in the backend.'
 	exit 1
 
 on/aws:
 	@echo 'enabling build-on aws feature flag (build AMI)'
 	# ToDo: set AWS creds with the feature flag.
-	export ON_AWS=1
+	export BUILD_ON_PARALLELS=1
 	@echo 'not implemented in the backend.'
 	exit 1
 
 on/azure:
 	@echo 'enabling build-on vmware feature flag'
 	#ToDo: Set the azure creds with the feature flag.
-	export ON_AZURE=1
+	export BUILD_ON_PARALLELS=1
 	@echo 'not implemented in the backend.'
 	exit 1
 
 on/parallels:
 	@echo 'enabling build-on parallels feature flag'
-	export ON_PARALLELS=1
+	export BUILD_ON_PARALLELS=1
+	@echo 'not implemented in the backend.'
+	exit 1
+
+to/local:
+	@echo 'enabling push to/local feature flag'
+	export PUSH_TO_LOCAL=1
 	@echo 'not implemented in the backend.'
 	exit 1
 
 to/vagrantup:
-	@echo 'enabling push-to vagrantup feature flag'
-	export TO_VAGRANTUP=1
+	@echo 'enabling push to/vagrantup feature flag'
+	export PUSH_TO_LOCAL=1
 	@echo 'not implemented in the backend.'
 	exit 1
