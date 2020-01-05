f076fc207c3f144e28ede726a19a3732a687de89
Sam Caldwell <mail@samcaldwell.net>
2020-01-05 09:58:32 -0600

    add feature_flags.mk

---
+++ Makefile.in/feature_flags.mk
@@ -0,0 +1,9 @@
+#
+# This is the current set of enabled feature flags.
+#
+-include ff-on-aws.mk
+-include ff-on-azure.mk
+-include ff-on-parallels.mk
+-include ff-on-virtualbox.mk
+-include ff-on-vmware.mk
+-include ff-to-vagrantup.mk
