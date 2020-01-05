8f894da1a88177c5060b685269c69ae1eccb2040
Sam Caldwell <mail@samcaldwell.net>
2020-01-05 16:36:52 -0600

    add --force to fetch-all

--- Makefile.in/fetch-all.mk
+++ Makefile.in/fetch-all.mk
@@ -1,27 +1,27 @@
 MANIFEST='$(shell pwd)/assets/manifest.yml'
 fetch/iso:
 	@echo 'fetch iso images in $(MANIFEST)'
-	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type iso --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py $(USE_FORCE) --type iso --manifest $(MANIFEST)
 
 fetch/windows:
 	@echo 'fetch windows assets from $(MANIFEST)'
-	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type windows --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py $(USE_FORCE) --type windows --manifest $(MANIFEST)
 
 fetch/linux/deb:
 	MANIFEST=assets/linux/deb/manifest.yaml
-	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type linux/deb --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py $(USE_FORCE) --type linux/deb --manifest $(MANIFEST)
 
 fetch/linux/rpm:
 	MANIFEST=assets/linux/rpm/manifest.yaml
-	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type linux/rpm --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py $(USE_FORCE) --type linux/rpm --manifest $(MANIFEST)
 
 fetch/linux/src:
 	MANIFEST=assets/linux/src/manifest.yaml
-	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type linux/src --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py $(USE_FORCE) --type linux/src --manifest $(MANIFEST)
 
 fetch/macos:
 	MANIFEST=assets/macos/manifest.yaml
-	python3 $(shell pwd)/scripts/packer/fetch-manifest.py --type macos --manifest $(MANIFEST)
+	python3 $(shell pwd)/scripts/packer/fetch-manifest.py $(USE_FORCE) --type macos --manifest $(MANIFEST)
 
 fetch/all:
 	@echo 'fetching all assets.'
