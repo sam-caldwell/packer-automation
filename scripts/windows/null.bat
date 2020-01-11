86876d71b4a6db296bd3af0825c27b6e1f412197
Sam Caldwell <mail@samcaldwell.net>
2020-01-11 10:02:07 -0600

    add the null.bat (default feature_install_script)

---
+++ scripts/windows/null.bat
@@ -0,0 +1,8 @@
+::
+:: scripts/null.bat
+:: (c) 2020 Sam Caldwell.  LICENSE.bat
+::
+:: This is the default (noop) feature_install_script.
+::
+
+@echo 'null.bat: a placeholder for feature_install_script'
