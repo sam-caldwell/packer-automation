f9b2aabd1d04fc71417c46d92fadd50b50b06b33
Sam Caldwell <mail@samcaldwell.net>
2020-01-12 12:31:49 -0600

    initial somewhat working windows/10 test.

--- Makefile.in/test/windows/10/test.mk
+++ Makefile.in/test/windows/10/test.mk
@@ -1,3 +1,6 @@
 test/windows/10:
 	@echo 'test/windows/10'
+	@$(MAKE) to/local push/boxes
 	@/bin/bash -c "( (cd ./Makefile.in/test/windows/10; vagrant up ) )"
+	@/bin/bash -c "( (cd ./Makefile.in/test/windows/10; vagrant ssh 'choco /?' ) )"
+	@/bin/bash -c "( (cd ./Makefile.in/test/windows/10; vagrant ssh 'choco install /y python3' ) )"
