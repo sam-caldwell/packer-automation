bbc03b75b0ff903d22b626602b2172690bbf205a
Sam Caldwell <mail@samcaldwell.net>
2020-01-11 23:33:25 -0600

    add retry logic in install7z installer batch file.

--- scripts/windows/install7z.bat
+++ scripts/windows/install7z.bat
@@ -1,4 +1,5 @@
 if not exist "C:\windows\Temp\7z920-x64.msi" (
-	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z920-x64.msi', 'C:\windows\Temp\7z920-x64.msi')" <NUL
+	powershell -Command "do{sleep 5;(New-Object Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z920-x64.msi','C:\windows\Temp\7z920-x64.msi')}while(!$?)" <NUL
 )
 msiexec /qb /i C:\windows\Temp\7z920-x64.msi
+
